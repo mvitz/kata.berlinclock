392ad6097b358fc9e722f62774f39a21e6c6b58f
Michael Vitz <vitz.michael@gmail.com>
2014-04-04 22:51:26 +0200

    Top hours of berlin clock works.

--- src/main/java/de/mvitz/kata/berlinclock/BerlinClock.java
+++ src/main/java/de/mvitz/kata/berlinclock/BerlinClock.java
@@ -1,9 +1,18 @@
 package de.mvitz.kata.berlinclock;
 
+import static de.mvitz.kata.berlinclock.BerlinClock.Lamp.OFF;
+import static de.mvitz.kata.berlinclock.BerlinClock.Lamp.RED;
+import static de.mvitz.kata.berlinclock.BerlinClock.Lamp.YELLOW;
+import static java.util.concurrent.TimeUnit.HOURS;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.concurrent.TimeUnit;
+
 public class BerlinClock {
 
     public static enum Lamp {
-        YELLOW("Y"), OFF("O");
+        YELLOW("Y"), RED("R"), OFF("O");
         private final String mnemonic;
 
         private Lamp(final String mnemonic) {
@@ -16,14 +25,14 @@ public class BerlinClock {
         }
     }
 
-    private int seconds;
+    private long seconds;
 
-    public BerlinClock(final int seconds) {
-        this.seconds = seconds;
+    public BerlinClock(final int hours, final int minutes, final int seconds) {
+        this.seconds = HOURS.toSeconds(hours) + MINUTES.toSeconds(minutes) + seconds;
     }
 
     Lamp getSeconds() {
-        return seconds % 2 == 0 ? Lamp.YELLOW : Lamp.OFF;
+        return glow(YELLOW, seconds % 2 == 0);
     }
 
     public void tick() {
@@ -31,7 +40,24 @@ public class BerlinClock {
     }
 
     void tick(final int seconds) {
-        this.seconds = (this.seconds + seconds) % 60;
+        tick(seconds, SECONDS);
+    }
+
+    void tick(final int duration, final TimeUnit unit) {
+        this.seconds += unit.toSeconds(duration);
+    }
+
+    Lamp[] getTopHours() {
+        final long hours = SECONDS.toHours(seconds) % 24;
+        return new Lamp[] {
+                glow(RED, hours > 4),
+                glow(RED, hours > 9),
+                glow(RED, hours > 14),
+                glow(RED, hours > 19) };
+    }
+
+    private Lamp glow(final Lamp lamp, final boolean expression) {
+        return expression ? lamp : OFF;
     }
 
 }
--- src/test/java/de/mvitz/kata/berlinclock/BerlinClockTest.java
+++ src/test/java/de/mvitz/kata/berlinclock/BerlinClockTest.java
@@ -1,31 +1,64 @@
 package de.mvitz.kata.berlinclock;
 
+import static de.mvitz.kata.berlinclock.BerlinClock.Lamp.OFF;
+import static de.mvitz.kata.berlinclock.BerlinClock.Lamp.RED;
+import static de.mvitz.kata.berlinclock.BerlinClock.Lamp.YELLOW;
+import static java.util.concurrent.TimeUnit.HOURS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
-import de.mvitz.kata.berlinclock.BerlinClock.Lamp;
-
 public class BerlinClockTest {
+
+    private final BerlinClock clock = new BerlinClock(0, 0, 0);
 
     @Test
     public void secondLampIsOnEveryTwoSeconds() throws Exception {
-        final BerlinClock clock = new BerlinClock(0);
-        assertThat(clock.getSeconds()).isEqualTo(Lamp.YELLOW);
-        clock.tick(); // 1
-        assertThat(clock.getSeconds()).isEqualTo(Lamp.OFF);
-        clock.tick(); // 2
-        assertThat(clock.getSeconds()).isEqualTo(Lamp.YELLOW);
-        clock.tick(2); // 4
-        assertThat(clock.getSeconds()).isEqualTo(Lamp.YELLOW);
-        clock.tick(3); // 7
-        assertThat(clock.getSeconds()).isEqualTo(Lamp.OFF);
-        clock.tick(52); // 59
-        assertThat(clock.getSeconds()).isEqualTo(Lamp.OFF);
-        clock.tick(); // 0
-        assertThat(clock.getSeconds()).isEqualTo(Lamp.YELLOW);
-        clock.tick(); // 1
-        assertThat(clock.getSeconds()).isEqualTo(Lamp.OFF);
+        assertThat(clock.getSeconds()).as("Seconds: %s", 0).isEqualTo(YELLOW);
+        clock.tick();
+        assertThat(clock.getSeconds()).as("Seconds: %s", 1).isEqualTo(OFF);
+        clock.tick();
+        assertThat(clock.getSeconds()).as("Seconds: %s", 2).isEqualTo(YELLOW);
+        clock.tick(2);
+        assertThat(clock.getSeconds()).as("Seconds: %s", 4).isEqualTo(YELLOW);
+        clock.tick(3);
+        assertThat(clock.getSeconds()).as("Seconds: %s", 7).isEqualTo(OFF);
+        clock.tick(52);
+        assertThat(clock.getSeconds()).as("Seconds: %s", 59).isEqualTo(OFF);
+        clock.tick();
+        assertThat(clock.getSeconds()).as("Seconds: %s", 0).isEqualTo(YELLOW);
+        clock.tick();
+        assertThat(clock.getSeconds()).as("Seconds: %s", 1).isEqualTo(OFF);
     }
 
+    @Test
+    public void topHourLampAddedEveryFiveHours() throws Exception {
+        // 00 - 04
+        for (int i = 0; i < 5; i++) {
+            assertThat(clock.getTopHours()).as("Hour: %s", i).containsExactly(OFF, OFF, OFF, OFF);
+            clock.tick(1, HOURS);
+        }
+        // 05 - 09
+        for (int i = 5; i < 10; i++) {
+            assertThat(clock.getTopHours()).as("Hour: %s", i).containsExactly(RED, OFF, OFF, OFF);
+            clock.tick(1, HOURS);
+        }
+        // 10 - 14
+        for (int i = 10; i < 15; i++) {
+            assertThat(clock.getTopHours()).as("Hour: %s", i).containsExactly(RED, RED, OFF, OFF);
+            clock.tick(1, HOURS);
+        }
+        // 15 - 19
+        for (int i = 15; i < 20; i++) {
+            assertThat(clock.getTopHours()).as("Hour: %s", i).containsExactly(RED, RED, RED, OFF);
+            clock.tick(1, HOURS);
+        }
+        // 20 - 23
+        for (int i = 20; i < 24; i++) {
+            assertThat(clock.getTopHours()).as("Hour: %s", i).containsExactly(RED, RED, RED, RED);
+            clock.tick(1, HOURS);
+        }
+        // 24 -> 00
+        assertThat(clock.getTopHours()).as("Hour: 24").containsExactly(OFF, OFF, OFF, OFF);
+    }
 }
